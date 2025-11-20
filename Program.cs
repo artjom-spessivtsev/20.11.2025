6f762b721fd43dab5d968cb03534fcb3ab70b02e
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-11-20 10:38:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SwitchryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try Catch koos Switch-ga");
+
+            //alguses tha switch kus saab sisestada numbreid 1 ja 2
+            int input = int.Parse(Console.ReadLine());
+
+            try
+            {
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Sisestasid number 1");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Sisestasid number 2");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta kehtiv number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Tekkis ootamatu viga: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿namespace whileDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("While Do");
+
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                //i++ t´hendab, et sama mis i = i + 1
+                i++;
+            }  while(i <= 100);
+        }
+    }
+}
